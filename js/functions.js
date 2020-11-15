679a2b2b0e05534a15e24d70a8a707c1b1d11090
aeronalmira0005 <69041811+aeronalmira0005@users.noreply.github.com>
2020-11-15 16:12:53 +0800

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,45 @@
 $(document).ready(function(){
-  $(".left-car").attr("src","images/default_car.png")
-  $(".right-car").attr("src","images/default_car.png")
+  $(".left-car").attr("src","images/default_car.png");
+  $(".right-car").attr("src","images/default_car.png");
 });
 
-$("#leftCarColor").change(function(){
-  var leftCarColor = $(this).children("option:selected").val();
+
+$(document).on('change', '#leftCarColor', function(e) {
+    var color = this.options[e.target.selectedIndex].text;
+
+    if (color === "Blue"){
+      $(".left-car").attr("src","images/blue_car.png");
+    }
+
+    else if (color === "Red"){
+      $(".left-car").attr("src","images/red_car.png");
+    }
+
+    else if (color === "Green"){
+      $(".left-car").attr("src","images/green_car.png");
+    }
+
+    else{
+      $(".left-car").attr("src","images/default_car.png");
+    }
+});
+
+$(document).on('change', '#rightCarColor', function(e) {
+    var color = this.options[e.target.selectedIndex].text;
+
+    if (color === "Blue"){
+      $(".right-car").attr("src","images/blue_car.png");
+    }
+
+    else if (color === "Red"){
+      $(".right-car").attr("src","images/red_car.png");
+    }
+
+    else if (color === "Green"){
+      $(".right-car").attr("src","images/green_car.png");
+    }
+
+    else{
+      $(".right-car").attr("src","images/default_car.png");
+    }
 });
